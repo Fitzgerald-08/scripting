6b6fdcad32562d33186726fa5a8bafbacb2c083e
Fitzgerald-08 <epsilon.33@tutamail.com>
2026-08-23 20:55:46 -0700

    Change path variable

--- backup.sh
+++ backup.sh
@@ -2,25 +2,31 @@
 
 # The path to the directory where to search
 # Validate the input of the user to avoid unexpected behaviors
-path=$1
+input_path=$1
 backup_dir=$HOME/backups/
 backup_dir_contents=$HOME/backups/*
 
 # If the path provided DOES NOT contain a / at the end
 # add it and set it as the new value for the variable.
-if [[ ! $path =~ /$ ]]
+if [[ $# == 0 ]]
+then
+    echo "Provide at least the path or file name to backup"
+    exit
+fi
+
+if [[ ! $input_path =~ /$ ]]
 then
     # If the file is a directory, scan it and return every single
     # file stored in it.
-    if [ -d $path ]
+    if [ -d $input_path ]
     then
-        path="${path}/"
+        input_path="${input_path}/"
     # If it's a file, just create a backup copy of it and move
     # it to the backup folder.
-    elif [ -f $path ]
+    elif [ -f $input_path ]
     then
         # Extract the name of the file you wish to backup
-        filename=$(ls $path | tr "/" "\n" | tail -n 1)
+        filename=$(ls $input_path | tr "/" "\n" | tail -n 1)
         # Add the .bak extension (Important to keep a distinction and referene it later)
         compose_filename="${filename}.bak"
         # Scan the directory backups to look for the file in question
@@ -33,7 +39,7 @@ then
                 exit
             else
                 # If the find command found nothing, proceed to make the copy
-                cp $path "$backup_dir$compose_filename"
+                cp $input_path "$backup_dir$compose_filename"
                 # If the status code of the previous operation was 0, print a message
                 # showing it all went well, and an error message otherwise.
                 if (( $? == 0 ))
@@ -52,7 +58,7 @@ fi
 
 # In case the path provided is the same as that of the one
 # for backups, throw a message indicating so and exit.
-if [[ $path = $backup_dir ]]
+if [[ $input_path =~ $backup_dir ]]
 then
     echo "[!] Fatal."
     echo "Source and target directories are the same"
@@ -62,7 +68,7 @@ fi
 
 # If the first validation passes, scan the whole directory
 # and backup every single file to its dedicated backup folder
-for f in ${path}*
+for f in ${input_path}*
 do
     # If the file in quetion is a directory, skip this copy
     # and proceed with the next file.
